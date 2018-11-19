b5935a8c03e903db43bccf72d392437549965d57
clement2323 <guillo.clement@gmail.com>
2018-11-20 00:44:04 +0100

    test

--- creation_csv_a_partir_du_fichier_R_CG.R
+++ creation_csv_a_partir_du_fichier_R_CG.R
@@ -1,11 +1,14 @@
 
 library(data.table)
-#load("C:/Users/Clement/Desktop/Partage_virtual_box/blablacar/dataset/base_from_25-05-2018_to_01-09-2018.Rdata")
-load("W:/Blablacar/git/base_from_25-05-2018_to_01-09-2018_augmente_CG.Rdata")
+load("C:/Users/Clement/Desktop/Partage_virtual_box/blablacar/dataset/base_from_25-05-2018_to_01-09-2018_augmente_CG.Rdata")
+#load("W:/Blablacar/git/base_from_25-05-2018_to_01-09-2018_augmente_CG.Rdata")
 #load(W:/Blablacar/base/base_from_25-05-2018_to_01-09-2018.Rdata")
 #table(b$departure_city_name)
 ##Transformation des prix en numérique
 
+#length(unique(paste(base$requete_id,base$permanent_id)))
+#Ok on dégage beaucoup de doublon comme ça regarder les doubles si les lignes osnt bien identiques
+#Les dégager avant d'enregistrer le csv
 b$price<-sapply(b$price,function(x){
   #x<-btest$price[1]
   as.numeric(sub(substr(x,(nchar(x)-1),nchar(x)),"",x))
@@ -15,7 +18,6 @@ b$price_with_commision<-sapply(b$price_with_commision,function(x){
   #x<-btest$price_with_commision[1]
   as.numeric(sub(",",".",sub(substr(x,(nchar(x)-1),nchar(x)),"",x)))
 })
-
 #je vais me servir de l'id de la requête pour définir les variables
 #query_departure_city_name and query_arrival_city_name pour avoir les différences entre la ville demandée
 
@@ -28,3 +30,4 @@ fwrite(b,file="C:/Users/Clement/Desktop/Partage_virtual_box/blablacar/dataset/ba
 
 ###Essayer defaire une table associant depcom aux libellés de nom de ville
 ###idées de stat autour des cartes .
+#
